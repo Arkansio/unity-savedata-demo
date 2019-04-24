7cdbd034b78a52ff14deb8e4bb510026ebe34b26
Arkansio <38956983+Arkansio@users.noreply.github.com>
2019-04-25 01:05:32 +0200

    add example

---
+++ Assets/Scripts/dataManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class dataManager
+{
+    private static int defaultMoney = 700;
+    private static int actualMoney;
+
+    static dataManager() {
+        actualMoney = PlayerPrefs.GetInt("money", defaultMoney);
+    }
+
+    public static int setMoney(int val) {
+        PlayerPrefs.SetInt("money", val);
+        actualMoney = val;
+        return actualMoney;
+    }
+
+    public static int addMoney(int val) {
+        setMoney(actualMoney + val);
+        return actualMoney;
+    }
+
+    public static int delMoney(int val) {
+        setMoney(actualMoney - val);
+        return actualMoney;
+    }
+
+    public static int getMoney() {
+        return actualMoney;
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Money: " + dataManager.getMoney());
+        Debug.Log("Increase money: " + dataManager.addMoney(500));
+        Debug.Log("Decrease money: " + dataManager.delMoney(1000));
+        Debug.Log("Set money: " + dataManager.setMoney(400));
+    }
+}
